969f1e810e6a067d9f0794fd45ee4bc6814a557d
progman246 <hossein.abdi7@gmail.com>
2020-09-08 20:38:19 +0430

    define calculator class

---
+++ TryAgain/Classes/calculator.cs
@@ -0,0 +1,28 @@
+
+    public class Calculator1
+    {
+        //decleare field of class 
+        public double num1 {get;}
+        public double num2 {get;}
+
+        //decleare function of class 
+        public double add(double num1,double num2)
+        {
+            return num1+num2;
+        }
+
+         public double substract(double num1,double num2)
+        {
+            return num1-num2;
+        }
+
+         public double multiply(double num1,double num2)
+        {
+            return num1*num2;
+        }
+
+         public double division(double num1,double num2)
+        {
+            return num1/num2;
+        }
+    }
